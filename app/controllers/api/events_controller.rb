e1b3f7ba166bfb20ee179d043f2c4fa2ed59949b
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2019-07-15 19:44:10 -0400

    extract common Api logic into an API controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,9 +1,4 @@
-class Api::EventsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  rescue_from User::NotAuthorized, with: :not_authorized
-
-  skip_before_action :authenticate_user!
-
+class Api::EventsController < ApiController
   before_action :load_user, only: :create
   before_action :load_device, only: :create
 
@@ -18,24 +13,6 @@ class Api::EventsController < ApplicationController
   end
 
   protected
-
-  def load_user
-    raise User::NotAuthorized if params[:auth_token].blank?
-
-    @user = User.find_by!(auth_token: params[:auth_token])
-  end
-
-  def load_device
-    @device = @user.devices.find_by!(uuid: params[:device])
-  end
-
-  def record_not_found
-    render json: { error: 'Record not found' }, status: :not_found
-  end
-
-  def not_authorized
-    render json: { error: 'Not authorized' }, status: :unauthorized
-  end
 
   def event_params
     params.require(:event).permit(
---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,26 @@
+class ApiController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from User::NotAuthorized, with: :not_authorized
+
+  skip_before_action :authenticate_user!
+
+  protected
+
+  def record_not_found
+    render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def not_authorized
+    render json: { error: 'Not authorized' }, status: :unauthorized
+  end
+
+  def load_user
+    raise User::NotAuthorized if params[:auth_token].blank?
+
+    @user = User.find_by!(auth_token: params[:auth_token])
+  end
+
+  def load_device
+    @device = @user.devices.find_by!(uuid: params[:device])
+  end
+end
