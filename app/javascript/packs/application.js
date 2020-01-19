ff2059ce503aa460c19703367cdf0bb3b462239d
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2020-01-19 01:23:53 -0500

    Replace Bootstrap with Tailwind (#16)
    * replace bootstrap with tailwind
    
    * convert navbar & login form to tailwind styles
    
    * remove popper.js & jquery from project
    
    * add a skeleton tailwind config
    
    * convert user profile form to use Tailwind styles
    
    * remove octicons gem
    
    * convert device form to Tailwind styles
    
    * fix hover class typo for device in navbar
    
    * convert sign-up form to Tailwind styles
    
    * fix indentation for devise form
    
    * reverse location of login / signup buttons
    
    * convert events listing page to tailwind styles
    
    * change when device UUIDs are generated
    
    * add partial for flash notices
    
    * move header partial under views/shared
    
    * convert device listing page to use Tailwind styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,4 +19,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require('bootstrap')
+import('tailwindcss/base')
+import('tailwindcss/components')
+import('tailwindcss/utilities')
+
+require('../../assets/stylesheets/application.scss')
