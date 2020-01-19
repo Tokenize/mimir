ef8b34f0880f858c2d6af7c0ff147c5e4d7e8409
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2020-01-19 00:48:24 -0500

    convert device listing page to use Tailwind styles

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -1,2 +1,11 @@
 module DevicesHelper
+  def device_last_seen(device)
+    return 'has not been seen yet' if device.last_seen_at.nil?
+
+    "last seen #{time_ago_in_words(device.last_seen_at)} ago"
+  end
+
+  def newest_events_for_device(device)
+    device.events.order(timestamp: :desc).limit(5)
+  end
 end
