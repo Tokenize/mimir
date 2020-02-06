6643025956928480d8a6a0a2e676b54fe12c7d20
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2020-02-05 23:55:55 -0500

    configure action mailer for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/deploy/apps/mimir"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", ".bundle"
