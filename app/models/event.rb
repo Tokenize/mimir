c0aea68c43b49ae6c67a4e729c13b2ef2f8d53bc
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2020-08-10 18:50:40 -0400

    make new events update the device's last_seen_at

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
   has_one :user, through: :device
 
   validates :command, :description, :device_id, :timestamp, presence: true
+
+  after_create :update_device_last_seen_at
+
+  private
+
+  def update_device_last_seen_at
+    device.update!(last_seen_at: Time.current)
+  end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -28,4 +28,16 @@ class EventTest < ActiveSupport::TestCase
     assert_not event.valid?
     assert event.errors[:timestamp].present?
   end
+
+  test "after_create callback updates device's last_seen_at" do
+    device = devices(:one)
+    original_last_seen_at = device.last_seen_at
+
+    travel_to(5.minutes.from_now) do
+      event = device.events.create!(events(:one).attributes.merge(id: nil))
+      device.reload
+
+      assert_not_equal(original_last_seen_at, device.last_seen_at)
+    end
+  end
 end
