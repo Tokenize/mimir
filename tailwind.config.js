e645d0fdca2f22f11d3b58e50881d71bac7d89cd
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2021-03-29 18:11:26 -0400

    update Tailwind to 2.0.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.gray,
+      red: colors.red,
+      teal: colors.teal,
+      white: colors.white,
+      black: colors.black,
+    }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
